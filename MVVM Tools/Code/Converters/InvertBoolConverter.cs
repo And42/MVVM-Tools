d4ee3f90a04c245d328a453c18c1eee4685647fd
And42 <andrewskobcov@yandex.ru>
2018-01-10 20:54:48 +0300

    - Switched 'internal' -> 'public' in InvertBoolConverter

--- MVVM Tools/Code/Converters/InvertBoolConverter.cs
+++ MVVM Tools/Code/Converters/InvertBoolConverter.cs
@@ -3,7 +3,7 @@ using MVVM_Tools.Code.Classes;
 
 namespace MVVM_Tools.Code.Converters
 {
-    internal class InvertBoolConverter : ConverterBase<bool, bool>
+    public class InvertBoolConverter : ConverterBase<bool, bool>
     {
         protected override bool ConvertInternal(bool value, object parameter, CultureInfo culture)
         {
