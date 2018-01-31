49736a98d14f00424e8e45b47943c763115372df
And42 <andrewskobcov@yandex.ru>
2018-01-31 14:30:43 +0300

    - Added custom disposable class

---
+++ MVVM Tools/Code/Disposables/CustomBoolDisposable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MVVM_Tools.Code.Disposables
+{
+    /// <summary>
+    /// Class that sets the value to true on load and to false on dispose
+    /// </summary>
+    public class CustomBoolDisposable : IDisposable
+    {
+        private Action<bool> _setValue;
+
+        /// <summary>
+        /// Creates new instance of a class with the provided value setter
+        /// </summary>
+        /// <param name="setValue">Value setter</param>
+        /// <exception cref="ArgumentNullException">thrown if setValue is null</exception>
+        public CustomBoolDisposable(Action<bool> setValue)
+        {
+            if (setValue == null)
+                throw new ArgumentNullException(nameof(setValue));
+
+            _setValue = setValue;
+
+            _setValue(true);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            if (_setValue == null)
+                throw new ObjectDisposedException(nameof(CustomBoolDisposable));
+
+            _setValue(false);
+
+            _setValue = null;
+        }
+    }
+}
